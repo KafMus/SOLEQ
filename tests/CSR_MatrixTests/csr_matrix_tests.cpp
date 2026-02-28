f9bf489fbe6a41f689fb7eff3f24e39b15ae59ba
KafMus <kaflanov.mg@phystech.edu>
2026-02-28 03:05:32 +0300

    GTest applied for Matrix and CSR_Matrix.

---
+++ tests/CSR_MatrixTests/csr_matrix_tests.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include <SOLEQ/csr_matrix.hpp>
+#include <list>
+#include <utility>
+
+TEST(CSRMatrixInitialization, DefaultInitialization) {
+    kfsoleq::CSR_Matrix void_matrix{};
+    EXPECT_EQ(void_matrix.getValues().size(), 0) << "Matrix's Values size doesn't match";
+    EXPECT_EQ(void_matrix.getValues().capacity(), 0) << "Matrix's Values capacity doesn't match";
+    EXPECT_EQ(void_matrix.getColumnIndexes().size(), 0) << "Matrix's Column Indexes size doesn't match";
+    EXPECT_EQ(void_matrix.getColumnIndexes().capacity(), 0) << "Matrix's Column Indexes capacity doesn't match";
+    EXPECT_EQ(void_matrix.getRowIndexes().size(), 0) << "Matrix's Row Indexes size doesn't match";
+    EXPECT_EQ(void_matrix.getRowIndexes().capacity(), 0) << "Matrix's Row Indexes capacity doesn't match";
+}
+
+TEST(CSRMatrixInitialisation, LILInitialisation) {
+    std::list<std::pair<size_t, SOLEQ_FLOAT>>  lil_first_row = { std::make_pair(0, 1) };
+    std::list<std::pair<size_t, SOLEQ_FLOAT>> lil_second_row = { std::make_pair(1, 2) };
+    std::list<std::list<std::pair<size_t, SOLEQ_FLOAT>>> my_lil = { lil_first_row, lil_second_row };
+    
+    kfsoleq::CSR_Matrix my_matrix(my_lil);
+    EXPECT_EQ(my_matrix.getValues()[0], 1) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[1], 2) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[0], 0) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[1], 1) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[0], 0) << "Matrix's Row Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[1], 1) << "Matrix's Row Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[2], 2) << "Matrix's Row Indexes values doesn't match";
+    
+    
+    std::list<std::pair<size_t, SOLEQ_FLOAT>> lil_third_row{};
+    lil_first_row =  { std::make_pair(0, 1), std::make_pair(1, 2), std::make_pair(3, 3) };
+    lil_second_row = { std::make_pair(2, 4) };
+    lil_third_row =  { std::make_pair(1, 1), std::make_pair(3, 11) };
+    my_lil = { lil_first_row, lil_second_row, lil_third_row };
+    
+    my_matrix = kfsoleq::CSR_Matrix(my_lil);
+    EXPECT_EQ(my_matrix.getValues()[0], 1) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[1], 2) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[2], 3) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[3], 4) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[4], 1) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getValues()[5], 11) << "Matrix's Values values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[0], 0) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[1], 1) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[2], 3) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[3], 2) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[4], 1) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getColumnIndexes()[5], 3) << "Matrix's Column Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[0], 0) << "Matrix's Row Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[1], 3) << "Matrix's Row Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[2], 4) << "Matrix's Row Indexes values doesn't match";
+    EXPECT_EQ(my_matrix.getRowIndexes()[3], 6) << "Matrix's Row Indexes values doesn't match";
+}
---
+++ tests/MatrixTests/matrix_tests.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+#include <SOLEQ/matrix.hpp>
+#include <vector>
+
+TEST(MatrixInitialization, DefaultInitialization) {
+    kfsoleq::Matrix void_matrix{};
+    EXPECT_EQ(void_matrix.getSizeY(), 0) << "Matrix's Y size doesn't match";
+    EXPECT_EQ(void_matrix.getSizeX(), 0) << "Matrix's X size doesn't match";
+    EXPECT_EQ(void_matrix.getValues().size(), 0) << "Matrix's Values size doesn't match";
+    EXPECT_EQ(void_matrix.getValues().capacity(), 0) << "Matrix's Values capacity doesn't match";
+}
+
+TEST(MatrixInitialisation, SizeBySizeInitialisation) {
+    kfsoleq::Matrix my_matrix;
+    std::vector<SOLEQ_FLOAT> my_matrix_values;
+    size_t size_y[6] = { 0, 3, 3,  5000,  1,     10000 };
+    size_t size_x[6] = { 0, 3, 10, 10000, 10000, 1     };
+    
+    for (size_t i = 0; i < 6; ++i) {
+        my_matrix = kfsoleq::Matrix(size_y[i], size_x[i]);
+        EXPECT_EQ(my_matrix.getSizeY(), size_y[i]) << "Matrix's Y size doesn't match";
+        EXPECT_EQ(my_matrix.getSizeX(), size_x[i]) << "Matrix's X size doesn't match";
+        EXPECT_EQ(my_matrix.getValues().size(), size_y[i] * size_x[i]) << "Matrix's Values size doesn't match";
+        EXPECT_EQ(my_matrix.getValues().capacity(), size_y[i] * size_x[i]) << "Matrix's Values capacity doesn't match";
+        
+        my_matrix_values = my_matrix.getValues();
+        for (size_t j = 0; j < size_y[i]; ++j) {
+            for (size_t z = 0; z < size_x[i]; ++z) {
+                EXPECT_EQ(my_matrix_values[j * size_x[i] + z], 0) << "Matrix's Values values doesn't match";
+            }
+        }
+    }
+}
+
+TEST(MatrixOperators, AccessOperator) {
+    kfsoleq::Matrix my_matrix(10, 30);
+    std::vector<SOLEQ_FLOAT> my_matrix_values;
+    SOLEQ_FLOAT salt_num = 11037;
+    
+    for (size_t i = 0; i < 10; ++i) {
+        for (size_t j = 0; j < 30; ++j) {
+            my_matrix(i, j) = (salt_num * (double)(i * j)) + (double)i;
+        }
+    }
+    
+    for (size_t i = 0; i < 10; ++i) {
+        for (size_t j = 0; j < 30; ++j) {
+            EXPECT_EQ(my_matrix(i, j), (salt_num * (double)(i * j)) + (double)i) << "Matrix's Values values doesn't match";
+        }
+    }
+    
+    my_matrix_values = my_matrix.getValues();
+    for (size_t i = 0; i < 10; ++i) {
+        for (size_t j = 0; j < 30; ++j) {
+            EXPECT_EQ(my_matrix_values[i * 30 + j], (salt_num * (double)(i * j)) + (double)i) << "Matrix's Values values doesn't match";
+        }
+    }
+}
